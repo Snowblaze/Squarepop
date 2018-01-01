7bfccba6853c961c1fbb08cd60d23efca3c30e37
Snowblaze <a.a.matinyan@gmail.com>
2018-01-01 20:42:11 +0400

    Board initialization with colors.

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    [SerializeField]
+    private int columns;
+    [SerializeField]
+    private int rows;
+    [SerializeField]
+    private Vector2 boardSize;
+    private Vector2 boardOffset;
+
+    [SerializeField]
+    private GameObject tilePrefab;
+    [SerializeField]
+    private Color[] tileColors;
+
+    private Vector2 tileSize;
+    private Vector2 tileScale;
+
+    public GameObject[,] tiles;
+
+    private void Awake()
+    {
+        if (tileColors == null) throw new Exception("BoardManager: colors array is null.");
+        InitializeColors(tileColors);
+    }
+
+    // Use this for initialization
+    private void Start () {
+        InitializeBoard();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private void InitializeBoard()
+    {
+        tiles = new GameObject[columns, rows];
+
+        tileSize = tilePrefab.GetComponent<SpriteRenderer>().sprite.bounds.size;
+
+        Vector2 newTileSize = new Vector2(boardSize.x / (float)columns, boardSize.y / (float)rows);
+        tileScale.x = newTileSize.x / tileSize.x;
+        tileScale.y = newTileSize.y / tileSize.y;
+
+        tileSize = newTileSize;
+
+        boardOffset.x = -(boardSize.x / 2) + tileSize.x / 2;
+        boardOffset.y = -(boardSize.y / 2) + tileSize.y / 2;
+
+        for (int col = 0; col < columns; col++)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                Vector2 pos = new Vector2(col * tileSize.x + boardOffset.x + transform.position.x, row * tileSize.y + boardOffset.y + transform.position.y);
+
+                GameObject newTile = Instantiate(tilePrefab, pos, Quaternion.identity, transform);
+                newTile.transform.localScale = tileScale;
+                tiles[col, row] = newTile;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, boardSize);
+    }
+
+    private void InitializeColors(Color[] colors)
+    {
+        TileScript.GenerateMaterials(colors);
+    }
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+    static List<Material> materials = new List<Material>();
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().material = GetRandomMaterial();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void GenerateMaterials(Color[] colors)
+    {
+        foreach (var color in colors)
+        {
+            var mat = new Material(Shader.Find("Sprites/Default"))
+            {
+                color = color
+            };
+            materials.Add(mat);
+        }
+    }
+
+    public Material GetRandomMaterial()
+    {
+        if (materials.Count == 0) throw new Exception("TileScript: no materials generated.");
+
+        int index = UnityEngine.Random.Range(0, materials.Count);
+        return materials[index];
+    }
+}
